b622e3324d63f85f087a21bb8f223d3345ffdee6
Jacob <jacob.wallace@utexas.edu>
2023-09-29 18:13:51 -0700

    raft_tile separate scene and script

--- objects/raft/raft.gd
+++ objects/raft/raft.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var raft_tile_height = 4
 @export var raft_starting_pos_x = 350
 @export var raft_starting_pos_y = 100
+var raft_tile_scene = preload("res://objects/raft_tile/raft_tile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,12 +19,13 @@ func generate_raft():
 		raft_pos_x = raft_starting_pos_x
 		
 		for c in range(raft_tile_length):
-			generate_raft_tile(Vector2(raft_pos_x, raft_pos_y))
+			generate_raft_tile(Vector2(raft_pos_x, raft_pos_y), r, c)
 			raft_pos_x += 32
 
-func generate_raft_tile(pos: Vector2):
-	var new_raft_tile = Sprite2D.new()
-	new_raft_tile.texture = preload("res://assets/textures/raft.png")
+func generate_raft_tile(pos: Vector2, row: int, column: int):
+	var new_raft_tile = raft_tile_scene.instantiate()
+	new_raft_tile.row_index = row
+	new_raft_tile.column_index = column
 	new_raft_tile.set_position(pos)
 	self.add_child(new_raft_tile)
 
---
+++ resources/raft_tile_data.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name RaftTileData
+
+@export var tile_type: int = 0
