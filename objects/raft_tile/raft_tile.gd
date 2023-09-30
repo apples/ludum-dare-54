06a0338cbcc1e1444e798eaf4645f99c62792a9b
Anthony Stewart <anthony.l.stewart@outlook.com>
2023-09-29 22:00:48 -0500

    Added raft_tile health

--- objects/raft_tile/raft_tile.gd
+++ objects/raft_tile/raft_tile.gd
@@ -1,13 +1,28 @@
-extends Node2D
+class_name raft_tile extends Node2D
 
-var row_index = 0
-var column_index = 0
+var row_index: int = 0
+var column_index: int = 0
+
+var health: int = 100 :
+	set = _set_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _set_health(value: int):
+	health = value
+	if health <= 0:
+		print("killed")
+		get_parent().remove_child(self)
+
+func damage(value: int):
+	print("damaged: %d, remaining: %d" % [value, health - value])
+	health -= value
+
+func heal(value: int):
+	health += value
