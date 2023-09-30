1af8c59d9970e3bb5c14bb92d112588913d2c637
Anthony Stewart <anthony.l.stewart@outlook.com>
2023-09-29 23:26:13 -0500

    Added raft_tile functions to get surrounding tiles

--- objects/raft/raft.gd
+++ objects/raft/raft.gd
@@ -1,9 +1,9 @@
-extends Node2D
+class_name Raft extends Node2D
 
-@export var raft_tile_length = 5
-@export var raft_tile_height = 4
-@export var raft_starting_pos_x = 0
-@export var raft_starting_pos_y = 0
+@export var raft_tile_length: int = 5
+@export var raft_tile_height: int = 4
+@export var raft_starting_pos_x: float = 0
+@export var raft_starting_pos_y: float = 0
 var raft_tile_scene = preload("res://objects/raft_tile/raft_tile.tscn")
 var raft_tile_cannon_scene = preload("res://objects/raft_tile/raft_tile_cannon.tscn")
 
@@ -16,14 +16,14 @@ func _ready():
 	
 func swap_tile(tile_scene: PackedScene, row: int, column: int):
 	var new_tile = tile_scene.instantiate()
-	new_tile.copy_properties(raft_data_structure[Vector2(row, column)])
-	raft_data_structure[Vector2(row, column)].queue_free()
-	raft_data_structure[Vector2(row, column)] = new_tile
+	new_tile.copy_properties(raft_data_structure[Vector2i(row, column)])
+	raft_data_structure[Vector2i(row, column)].queue_free()
+	raft_data_structure[Vector2i(row, column)] = new_tile
 	self.add_child(new_tile)
 
 func generate_raft():
-	var raft_pos_y = raft_starting_pos_y
-	var raft_pos_x = raft_starting_pos_x
+	var raft_pos_y: float = raft_starting_pos_y
+	var raft_pos_x: float = raft_starting_pos_x
 	
 	for r in range(raft_tile_height):
 		raft_pos_y += 32
@@ -42,9 +42,20 @@ func generate_raft_tile(pos: Vector2, row: int, column: int):
 	new_raft_tile.row_index = row
 	new_raft_tile.column_index = column
 	new_raft_tile.set_position(pos)
-	raft_data_structure[Vector2(row, column)] = new_raft_tile
+	raft_data_structure[Vector2i(row, column)] = new_raft_tile
 #	raft_data_structure.keys()
 	self.add_child(new_raft_tile)
+
+func get_tile(row: int, column: int) -> RaftTile:
+	return raft_data_structure.get(Vector2i(row, column))
+
+const north := Vector2i(0, -1)
+const south := Vector2i(0, 1)
+const west := Vector2i(-1, 0)
+const east := Vector2i(1, 0)
+
+func get_relative_tile(direction: Vector2i, tile: RaftTile) -> RaftTile:
+	return get_tile(tile.row_index + direction.x, tile.column_index + direction.y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- objects/raft_tile/raft_tile.gd
+++ objects/raft_tile/raft_tile.gd
@@ -1,10 +1,12 @@
-class_name raft_tile extends Node2D
+class_name RaftTile extends Node2D
 
 var row_index: int = 0
 var column_index: int = 0
 
 var health: int = 100 :
 	set = _set_health
+
+var raft: Raft = get_parent() as Raft
 
 func copy_properties(raft_tile: Node2D):
 	self.row_index = raft_tile.row_index
@@ -22,10 +24,46 @@ func _process(delta):
 func _set_health(value: int):
 	health = value
 	if health <= 0:
-		get_parent().remove_child(self)
+		queue_free()
 
 func damage(value: int):
 	health -= value
 
 func heal(value: int):
 	health += value
+
+func get_north_tile() -> RaftTile:
+	return raft.get_relative_tile(raft.north, self)
+
+func get_south_tile() -> RaftTile:
+	return raft.get_relative_tile(raft.South, self)
+
+func get_west_tile() -> RaftTile:
+	return raft.get_relative_tile(raft.west, self)
+
+func get_east_tile() -> RaftTile:
+	return raft.get_relative_tile(raft.east, self)
+
+func get_surrounding_tiles() -> Array[RaftTile]:
+	var array: Array[RaftTile] = []
+
+	var north_tile = get_north_tile()
+	if north_tile != null:
+		array.append(north_tile)
+
+	var south_tile = get_south_tile()
+	if north_tile != null:
+		array.append(north_tile)
+
+	var west_tile = get_west_tile()
+	if north_tile != null:
+		array.append(north_tile)
+
+	var east_tile = get_east_tile()
+	if north_tile != null:
+		array.append(north_tile)
+
+	return array
+
+func get_surrounding_tile_count() -> int:
+	return get_surrounding_tiles().size()
