5c2c24e67e2b9e39272e035adb0161be46e33239
Jacob <jacob.wallace@utexas.edu>
2023-09-30 12:54:15 -0700

    upgrade select screen start

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,11 +1,18 @@
 extends Node2D
-
+var upgrade_select_scene = preload("res://scenes/upgrade_select/upgrade_select.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+#	enemy_destroyed()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func enemy_destroyed():
+	$Player._change_state(PlayerCharacter.STATE_SIT)
+	var display_upgrade_select = upgrade_select_scene.instantiate()
+	self.add_child(display_upgrade_select)
