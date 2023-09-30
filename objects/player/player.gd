7c414ac841e2c991c9f786dc073b435d436a9f52
Apples <2352020+apples@users.noreply.github.com>
2023-09-29 20:42:51 -0500

    Added player sit

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -1,15 +1,34 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+
+@export var idle_sprite: Texture2D
+@export var sit_sprite: Texture2D
 
 var nearby_interactions = []
 
+var input_disabled := false
+
+var is_sitting: bool:
+	get:
+		return is_sitting
+	set(v):
+		is_sitting = v
+		if is_sitting:
+			$Sprite.texture = sit_sprite
+		else:
+			$Sprite.texture = idle_sprite
+
+
 func _process(delta):
-	pass
+	is_sitting = true
 
 
 func _physics_process(delta):
+	if input_disabled:
+		velocity = Vector2.ZERO
+		return
+	
 	var lr = Input.get_axis("left", "right")
 	var ud = Input.get_axis("up", "down")
 	var move_input = Vector2(lr, ud)
@@ -19,3 +38,4 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
+
