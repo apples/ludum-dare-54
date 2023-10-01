ef2a55dc164a8e9ad32c77cb3a88f921bc414a46
charlie hume <charliehume33@gmail.com>
2023-10-01 14:25:37 -0500

    animate gem sparkle on match

---
+++ objects/VFX/gem_sparkle/gem_sparkle.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$gem_sparkle_sprite.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_timer_timeout():
+	self.queue_free()
--- objects/raft_objects/gem.gd
+++ objects/raft_objects/gem.gd
@@ -1,5 +1,7 @@
 extends "res://objects/raft_objects/raft_object.gd"
 
+var gem_sparkle_scene = preload("res://objects/VFX/gem_sparkle/gem_sparkle.tscn")
+
 func get_kind() -> StringName:
 	return "gem"
 
@@ -8,6 +10,8 @@ func _process_unconnected(delta):
 	
 	if ball_nbors.size() == 0:
 		return
-	
 	for b in ball_nbors:
+		var gem_sparkle_scene_b = gem_sparkle_scene.instantiate()
+		b.get_parent().add_child(gem_sparkle_scene_b)
+		
 		b.queue_free()
