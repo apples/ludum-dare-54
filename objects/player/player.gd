b962dea38a4bc13fa284c060eb8b65b3528f9882
charlie hume <charliehume33@gmail.com>
2023-10-01 17:04:32 -0500

    add god mode
    spawn held items with 1 to 5 keys  for dev testing

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -8,4 +8,10 @@ func _get_player_input():
 	input.down = Input.is_action_pressed("down")
 	input.interact = Input.is_action_pressed("interact")
 	input.cancel = Input.is_action_pressed("cancel")
+	input.one=Input.is_action_pressed("one")
+	input.two=Input.is_action_pressed("two")
+	input.three=Input.is_action_pressed("three")
+	input.four=Input.is_action_pressed("four")
+	input.five=Input.is_action_pressed("five")
+	input.six=Input.is_action_pressed("six")
 	return input
