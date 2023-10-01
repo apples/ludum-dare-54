4a963f62b19599b1b70228e3e850c309e801a226
Jacob <jacob.wallace@utexas.edu>
2023-10-01 00:14:34 -0700

    bomb explosions

--- objects/raft_objects/bomb.gd
+++ objects/raft_objects/bomb.gd
@@ -1,4 +1,5 @@
 extends "res://objects/raft_objects/raft_object.gd"
+var bomb_explosion_scene = preload("res://objects/bomb_explosion/bomb_explosion.tscn")
 
 func get_kind() -> StringName:
 	return "bomb"
@@ -9,5 +10,16 @@ func _process_unconnected(delta):
 	if ball_nbors.size() == 0:
 		return
 	
+	var bomb_match = false
 	for b in ball_nbors:
+		bomb_match = true
 		b.queue_free()
+
+	if bomb_match:
+		print("boomy")
+		for n in 3:
+			generate_explosion()
+		
+func generate_explosion():
+	var bomb_explosion: Node2D = bomb_explosion_scene.instantiate()
+	get_parent().add_child(bomb_explosion)
