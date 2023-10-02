276217806bc3e188dbcec323c2b465d5dd92b5cd
Jacob <jacob.wallace@utexas.edu>
2023-10-02 00:50:50 -0700

    options menu caches saved sounds

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -6,7 +6,9 @@ var spawn_rate_max = 7.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass	
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound_effects"), linear_to_db(DATA_STORE.current.sfx_volume))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(DATA_STORE.current.music_volume))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(DATA_STORE.current.master_volume))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
