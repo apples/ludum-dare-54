bc796da4fb2a91c971832f3500a03b915c421b67
Adam Howard <ahoward0920@gmail.com>
2023-10-01 20:53:15 -0400

    Make the o module suck less

--- resources/raft_modules/fixed_tetrominoes/o_1up.gd
+++ resources/raft_modules/fixed_tetrominoes/o_1up.gd
@@ -4,13 +4,13 @@ static func get_structure():
 	# [x, _]
 	# [x, c]
 	return {
-		#Vector2i(-1, -1): base_tile(),
-		#Vector2i(0, -1): base_tile(),
+		Vector2i(-1, -1): base_tile(),
+		Vector2i(0, -1): base_tile(),
 		#Vector2i(1, -1): base_tile(),
-		#Vector2i(-1, 0): base_tile(),
-		Vector2i(0, 0): base_tile(),
-		Vector2i(1, 0): random_tile(),
+		Vector2i(-1, 0): base_tile(),
+		Vector2i(0, 0): random_tile(),
+		#Vector2i(1, 0): base_tile(),
 		#Vector2i(-1, 1): base_tile(),
-		Vector2i(0, 1): base_tile(),
-		Vector2i(1, 1): base_tile(),
+		#Vector2i(0, 1): base_tile(),
+		#Vector2i(1, 1): base_tile(),
 	}
