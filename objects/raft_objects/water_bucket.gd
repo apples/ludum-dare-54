622f8ce7539985aaadb80825de33435c4d61f8d9
WiredOverload <mike.everywhere@gmail.com>
2023-10-02 11:06:03 -0500

    fixed water bucket

--- objects/raft_objects/water_bucket.gd
+++ objects/raft_objects/water_bucket.gd
@@ -19,15 +19,15 @@ func _process_unconnected(delta):
 	var trigger_sound = false
 	for b in ball_nbors:
 		for i in self.raft.get_tile_row(b.grid_pos.y):
-			if(i.tile_object != null && i.tile_object.get_kind() == "fire"):
-				self.raft.set_tile(raft_tile_scene, b.grid_pos.y, i.grid_pos.x)
+			if(i != null && i.is_on_fire):
+				i.fire_health = 0
 				trigger_sound = true
 			splash = water_splash_scene.instantiate()
 			get_tree().get_root().add_child(splash)
 			splash.global_position = i.global_position
 		for i in self.raft.get_tile_column(b.grid_pos.x):
-			if(i.tile_object != null && i.tile_object.get_kind() == "fire"):
-				self.raft.set_tile(raft_tile_scene, i.grid_pos.y, b.grid_pos.x)
+			if(i != null && i.is_on_fire):
+				i.fire_health = 0
 				trigger_sound = true
 			splash = water_splash_scene.instantiate()
 			get_tree().get_root().add_child(splash)
@@ -39,6 +39,6 @@ func _process_unconnected(delta):
 			replace_with_gem()
 		else:
 			b.queue_free()
-
+	
 	if(trigger_sound):
 		splash.get_node("SFX").play()
